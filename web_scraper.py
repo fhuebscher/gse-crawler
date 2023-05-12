f2fa9830b858adc0bd87aa013dbde100db4f5614
Luke-lee-ratcliffe <133080450+Luke-lee-ratcliffe@users.noreply.github.com>
2023-05-12 19:45:39 +1000

    Create web_scraper.py

---
+++ web_scraper.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/My_Neighbor_Totoro"
+
+print(Get_info(url).get('section_titles'))
+
+
+def Get_info(url):
+  #Initialise Various Arrays to hold data for dictionary
+    links = []
+    text = []
+    titles = []
+    subtitles = []
+    section_titles = []
+    
+    #This can be changed for a basic html input file rather than a request
+    page = requests.get(url)
+
+    #Convert html doc content into a soup object
+    content = BeautifulSoup(page.content, "html.parser")
+
+    #grab title and subtitle information
+    title_content = content.find_all("h1")
+    subtitle_content = content.find_all("h2")
+    section_title_content = content.find_all("h3")
+
+    for each in title_content:
+      titles.append(each.text.strip())
+
+    for each in subtitle_content:
+      subtitles.append(each.text.strip())
+
+    for each in section_title_content:
+      section_titles.append(each.text.strip())
+
+
+    #Grab text info
+    text_content = content.find_all("p")
+    for each in text_content:
+      text.append(each.text.strip())
+
+    #Grab link info
+    link_content = content.find_all("a")
+    for each in link_content:
+      links.append(each.text.strip())
+
+      #Append all info to dictionary
+    dictionary  = {
+        "links": links,
+        "text" : text,
+        "titles" : titles,
+        "subtitles" : subtitles,
+        "section_titles" : section_titles
+        }
+
+
+
+    #Dictionary structure is a dictionary of array of arrays
+    return dictionary
