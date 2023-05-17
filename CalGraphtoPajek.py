c0bc5c44faa0e814b939e0ffeb6da6a2e5f80e7e
Pinak Limaye <limayepinak@gmail.com>
2023-05-17 12:54:52 +1000

    Created a python file to change the california graph to pajek format. This is so we can analyze it in gephi to show that our pagerank algorithm works

---
+++ CalGraphtoPajek.py
@@ -0,0 +1,15 @@
+with open("CaliforniaGraph",'r') as f:
+    with open("CalGraph.net",'w') as w:
+        nodeMode = True
+        w.write("*Vertices 9664\n")
+        for i in f.readlines():
+            a = i[:-1].split(' ')
+            if nodeMode and a[0] != 'n':
+                nodeMode = False
+                w.write("*Edges\n")
+                continue
+
+            if nodeMode:
+                w.write(str(int(a[1]) + 1) + f' "{a[2]}"\n')
+            else:
+                w.write(str(int(a[1]) + 1) + f' {int(a[2]) + 1}\n')
