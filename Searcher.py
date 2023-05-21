f41057b686870c6b1dbae84b4819e7a3f909dc7b
Pinak Limaye <limayepinak@gmail.com>
2023-05-21 12:55:04 +1000

    Changed searcher to have a TFinheritance function. The function allows for similar terms to be included in the tf, for example mathematics should be included in the search for the term maths

--- Searcher.py
+++ Searcher.py
@@ -10,11 +10,35 @@ def TFIDFPageRank(count, globcount, tfDict):
         idf = np.log(globcount/count)
         a = {}
         for i in tfDict:
-            if normMatrix[i] == 0:
+            if normMatrix[i] == 0 or tfDict[i] == 0:
                 a[i] = 0
                 continue
 
-            a[i] = (0.8*tfDict[i]*idf) + (0.2*normMatrix[i])
+            a[i] = (0.9*tfDict[i]*idf) + (0.1*normMatrix[i])
         return a
     except:
         return 0
+    
+
+def TFInheritance(trieNode):
+    if len(trieNode.children) < 1:
+        return trieNode.pageCount
+
+    if trieNode.is_end:
+        divisor = 1/len(trieNode.children)
+    else:
+        divisor = 1
+
+    a = defaultdict(float)
+    for i in trieNode.children:
+        d = TFInheritance(trieNode.children[i])
+        for x in d:
+            a[x] += divisor*d[x]
+
+    if not trieNode.is_end:
+        return a
+
+    for i in a:
+        trieNode.pageCount[i] = trieNode.pageCount[i] + (0.3*a[i])
+
+    return trieNode.pageCount
